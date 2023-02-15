f469a79abd1278364a0fab7e3925efc842c6564f
Mehmet Atakan Demircioğlu <atakandemircioglu@jotform.com>
2023-02-15 15:28:43 +0300

    Basic router added

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
 <?php
 
-echo 'Testing..';
+include_once('src/Router.php');
+
+$router = new Router(substr($_SERVER['REQUEST_URI'], strlen(dirname($_SERVER['SCRIPT_NAME']))), $_SERVER['REQUEST_METHOD']);
+
+$router->get('/', function () use ($router) {
+    $router->sendResponse(["message" => "homepage"], 200);
+});
+
+$router->get('/test', function () use ($router) {
+    $router->sendResponse(["message" => "test"], 200);
+});
