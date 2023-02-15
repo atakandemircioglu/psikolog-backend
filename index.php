e2978311e5db0fb1975bc8127a30612ae831cee4
Mehmet Atakan Demircioğlu <atakandemircioglu@jotform.com>
2023-02-15 15:46:33 +0300

    fix autoload

--- index.php
+++ index.php
@@ -6,6 +6,7 @@ error_reporting(E_ALL);
 
 include_once('src/Router.php');
 include_once('src/Controllers/TherapistController.php');
+include_once('src/TableDB/init.php');
 
 $router = new Router(substr($_SERVER['REQUEST_URI'], strlen(dirname($_SERVER['SCRIPT_NAME']))), $_SERVER['REQUEST_METHOD']);
 
