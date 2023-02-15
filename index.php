9adcc5d844e1a1e931bc408a0f6db0737ecea656
Mehmet Atakan Demircioğlu <atakandemircioglu@jotform.com>
2023-02-15 15:45:13 +0300

    errors opened

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <?php
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 include_once('src/Router.php');
 include_once('src/Controllers/TherapistController.php');
 
