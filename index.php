169c0ef776512b9365f3a8f9447cac60fa41fbe5
Mehmet Atakan Demircioğlu <atakandemircioglu@jotform.com>
2023-02-15 15:48:17 +0300

    fix

--- index.php
+++ index.php
@@ -7,6 +7,7 @@ error_reporting(E_ALL);
 include_once('src/Router.php');
 include_once('src/Controllers/TherapistController.php');
 include_once('src/TableDB/init.php');
+include_once('src/TableDB/JotForm.php');
 
 $router = new Router(substr($_SERVER['REQUEST_URI'], strlen(dirname($_SERVER['SCRIPT_NAME']))), $_SERVER['REQUEST_METHOD']);
 
