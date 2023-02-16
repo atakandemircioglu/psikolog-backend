e7d73fbf6ace3644852d36b8fb9e43d6608d9fa3
Mehmet Atakan Demircioğlu <atakandemircioglu@jotform.com>
2023-02-16 17:36:31 +0300

    TherapistModel :: fix

--- src/TableDB/Models/TherapistModel.php
+++ src/TableDB/Models/TherapistModel.php
@@ -2,6 +2,7 @@
 
 class TherapistModel extends Model
 {
+    protected static $tableName = "Psikolog Başvuru Formu";
     protected static $tableID = "230433091943048";
     protected $primaryKey = "id";
     protected $fillables = [
