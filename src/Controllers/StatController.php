f4b34b610469a63e1a1e23ba49675f8bf0cba4ad
Mehmet Atakan Demircioğlu <atakandemircioglu@jotform.com>
2023-02-21 15:27:05 +0300

    Update StatController.php

--- src/Controllers/StatController.php
+++ src/Controllers/StatController.php
@@ -4,24 +4,41 @@ class StatController
 {
     public function getStats()
     {
-        //$therapistController = new TherapistController();
+        $therapistController = new TherapistController();
         $clientController = new ClientController();
 
-        echo "<pre>";
-        var_dump($clientController->getAllClients());
-        die;
+        $clients = $clientController->getAllClients();
 
-        // $content = [
-        //     'toplam_danisan' => 0,
-        //     'tedavisi_baslayan' => 0,
-        //     'tedavi_bekleyen' => 0,
-        //     'teavisi_tamamlanan' => 0,
-        //     'onay_bekleyen' => 0,
-        //     'onaylanan' => 0,
-        //     'toplam_psikolog' => count($therapistController->getAllTherapists()),
-        //     'gunluk_tamamlanan_randevu' => 0
-        // ];
+        $content = [
+            'toplam_danisan' => count($clients),
+            'tedavi_baslayan' => 0,
+            'tedavi_bekleyen' => 0,
+            'tedavi_tamamlanan' => 0,
+            'tedavi_iptal' => 0,
+            'onay_bekleyen' => 0,
+            'toplam_psikolog' => count($therapistController->getAllTherapists())
+        ];
 
+        foreach ($clients as $eachClient) {
+            switch ($eachClient['durum']) {
+                case 'Onay Bekliyor':
+                    $content['tedavi_bekleyen']++;
+                    break;
+                case 'Tedavisi Bitti':
+                    $content['tedavi_tamamlanan']++;
+                    break;
+                case 'Tedavisi Başladı':
+                    $content['tedavi_baslayan']++;
+                    break;
+                case 'İptal Edildi':
+                    $content['tedavi_iptal']++;
+                    break;
+                default:
+                    $content['tedavi_bekleyen']++;
+                    break;
+            }
+        }
 
+        return $content;
     }
 }
