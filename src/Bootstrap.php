d23e025426946d90e3172782ce30e106123cd32a
Mehmet Atakan Demircioğlu <atakandemircioglu@jotform.com>
2023-02-21 18:47:01 +0300

    Remove autoloader temp

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -13,23 +13,13 @@ class Bootstrap
 
     public function loadClasses()
     {
-        spl_autoload_register(function ($className) {
-            # Usually I would just concatenate directly to $file variable below
-            # this is just for easy viewing on Stack Overflow)
-            $ds = DIRECTORY_SEPARATOR;
-            $dir = __DIR__;
-
-            // replace namespace separator with directory separator (prolly not required)
-            $className = str_replace('\\', $ds, $className);
-
-            // get full name of file containing the required class
-            $file = "{$dir}{$ds}{$className}.php";
-
-            // get file if it is readable
-            if (is_readable($file)) {
-                require_once $file;
-            }
-        });
+        // TODO :: add autoloader
+        require_once('Controllers/StatController.php');
+        require_once('Controllers/TherapistController.php');
+        require_once('Controllers/ClientController.php');
+        require_once('TableDB/init.php');
+        require_once('TableDB/JotForm.php');
+        require_once('Router.php');
     }
 
     public function initCORS()
