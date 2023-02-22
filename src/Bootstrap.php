11d6fb8fa04c2d2ce2c2767fb7043b099e70575e
onuryuksell <onuryuksel@jotform.com>
2023-02-22 17:07:11 +0300

    Update Bootstrap.php

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -29,7 +29,7 @@ class Bootstrap
         header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
         header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
         header('Access-Control-Allow-Credentials: true');
-        header('Access-Control-Allow-Origin: http://localhost:3000/');
+        header('Access-Control-Allow-Origin: http://localhost:3000');
         header('Content-type: application/json; charset=utf-8');
     }
 
