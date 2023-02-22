3739701d2bb1d06bfce3abb5b3a4e01df3bb570a
onuryuksell <onuryuksel@jotform.com>
2023-02-22 10:39:43 +0300

    array fix

--- src/Controllers/UserController.php
+++ src/Controllers/UserController.php
@@ -3,10 +3,8 @@
 class UserController {
     public function login($username, $password) {
         $user = (new UserModel())->getByUsername($username);
-        var_dump($user);
-        return $user->username;
 
-        if ($user->password !== $password) {
+        if ($user["password"] !== $password) {
             return false;
         }
 
