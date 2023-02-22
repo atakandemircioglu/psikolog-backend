16f87da287cb800c5590220a990d235d4866d58b
onuryuksell <onuryuksel@jotform.com>
2023-02-22 16:23:47 +0300

    change code and new route for auth

--- routes.php
+++ routes.php
@@ -2,21 +2,21 @@
 
 $router->get('/therapists', function () use ($router) {
     if (!(new Auth())->isLoggedIn()) {
-        $router->sendResponse(['message' => 'Authentication Failed'], 403);
+        $router->sendResponse(['message' => 'Authentication Failed'], 401);
     }
     $router->sendResponse([(new TherapistController())->getAllTherapists($_GET)], 200);
 });
 
 $router->get('/clients', function () use ($router) {
     if (!(new Auth())->isLoggedIn()) {
-        $router->sendResponse(['message' => 'Authentication Failed'], 403);
+        $router->sendResponse(['message' => 'Authentication Failed'], 401);
     }
     $router->sendResponse([(new ClientController())->getAllClients($_GET)], 200);
 });
 
 $router->get('/client-options', function () use ($router) {
     if (!(new Auth())->isLoggedIn()) {
-        $router->sendResponse(['message' => 'Authentication Failed'], 403);
+        $router->sendResponse(['message' => 'Authentication Failed'], 401);
     }
     $clientController = new ClientController();
     $result = $clientController->getAllClients($_GET);
@@ -32,7 +32,7 @@ $router->get('/client-options', function () use ($router) {
 
 $router->post('/therapist-register', function () use ($router) {
     if (!(new Auth())->isLoggedIn()) {
-        $router->sendResponse(['message' => 'Authentication Failed'], 403);
+        $router->sendResponse(['message' => 'Authentication Failed'], 401);
     }
     try {
         $router->sendResponse([(new TherapistController())->onTherapistRegister($_REQUEST)], 200);
@@ -43,14 +43,14 @@ $router->post('/therapist-register', function () use ($router) {
 
 $router->get('/therapist-appointments', function () use ($router) {
     if (!(new Auth())->isLoggedIn()) {
-        $router->sendResponse(['message' => 'Authentication Failed'], 403);
+        $router->sendResponse(['message' => 'Authentication Failed'], 401);
     }
     $router->sendResponse([(new TherapistController())->getAllAppointments()], 200);
 });
 
 $router->get('/stats', function () use ($router) {
     if (!(new Auth())->isLoggedIn()) {
-        $router->sendResponse(['message' => 'Authentication Failed'], 403);
+        $router->sendResponse(['message' => 'Unauthorized'], 401);
     }
     $router->sendResponse([(new StatController())->getStats()], 200);
 });
@@ -58,4 +58,12 @@ $router->get('/stats', function () use ($router) {
 $router->post('/login', function () use ($router) {
     $body = json_decode(file_get_contents('php://input'), true);
     $router->sendResponse([(new UserController())->login($body['username'], $body['password'])], 200);
+});
+
+$router->get('/auth', function () use ($router) {
+    if (!(new Auth())->isLoggedIn()) {
+        $router->sendResponse(['message' => 'Authentication Failed'], 403);
+        return false;
+    }
+    $router->sendResponse(['message' => true], 200);
 });
