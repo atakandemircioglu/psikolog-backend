3e64c27cb3fbc310265fdb5162e6a7c447562960
onuryuksell <onuryuksel@jotform.com>
2023-02-22 10:38:03 +0300

    test 123

--- src/Controllers/UserController.php
+++ src/Controllers/UserController.php
@@ -4,7 +4,7 @@ class UserController {
     public function login($username, $password) {
         $user = (new UserModel())->getByUsername($username);
         var_dump($user);
-        return $user;
+        return $user->username;
 
         if ($user->password !== $password) {
             return false;
