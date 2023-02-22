73d81473170a284f45b7fde354e606e5e443e765
onuryuksell <onuryuksel@jotform.com>
2023-02-22 14:51:57 +0300

    delete double session_Start

--- src/Classes/Auth.php
+++ src/Classes/Auth.php
@@ -3,7 +3,6 @@
 class Auth {
     public function authenticate($user) {
         if (!$this->isLoggedIn()) {
-            session_start();
             $_SESSION["loggedIn"] = true;
             $_SESSION["username"] = $user["username"];
             $_SESSION["id"] = $user["id"];
